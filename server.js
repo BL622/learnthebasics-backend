ce0340bfb12909f030153235dd397958481e00ba
BL622 <baloghm.levente@gmail.com>
2024-01-23 17:24:20 +0100

    cyclic push test

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const gameRoutes = require('./routes/gameRoutes')
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/player', userRoutes);
+app.use('/game', gameRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello test koyeb push')
-})
+    res.send('Hello test koyeb push')
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.listen(PORT, function () {
+  console.log('Server running on http://localhost:' + PORT);
+});
