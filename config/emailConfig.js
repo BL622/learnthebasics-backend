5351e6112e55a0c17f66cafc33c4e309cd952b47
Balogh Levente <baloghm.levente@gmail.com>
2024-01-24 11:32:40 +0100

    agyrak draga token exp verify

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'diak.jedlik.eu',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
--- controllers/tokenGeneration.js
+++ controllers/tokenGeneration.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
 function generateToken(userId) {
-  return jwt.sign({ userId }, process.env.SECRET_KEY , { expiresIn: '1h' });
+  return jwt.sign({ userId }, process.env.SECRET_KEY , { expiresIn: '1m' });
 }
 
 module.exports = { generateToken };
