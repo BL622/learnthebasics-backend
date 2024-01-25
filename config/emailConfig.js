6fa856b50ce744bf0a16b29754354977de509174
Balogh Levente <95307172+BL622@users.noreply.github.com>
2024-01-25 08:18:20 +0100

    Update emailConfig.js

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
 const transporter = nodemailer.createTransport({
-    service: 'diak.jedlik.eu',
+    service: 'gmail',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
