f5987c2baae49e37cba325a698ecf8170fade1f5
jtothmark2 <jtothmark@gmail.com>
2024-02-27 14:46:48 +0100

    backend fix

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,136 +1,195 @@
-const { executeQuery, tryCatch, log, checkExistingSave, updateSave, createSave, createSaveIfNotExists } = require('../sharedFunctions/functions');
-const { decryptToken } = require('./tokenGeneration');
-
+const {
+  executeQuery,
+  tryCatch,
+  log,
+  checkExistingSave,
+  updateSave,
+  createSave,
+  createSaveIfNotExists,
+} = require("../sharedFunctions/functions");
+const { decryptToken } = require("./tokenGeneration");
 
 const gameController = {
+  getSaves: async function (req, res) {
+    const authCode = req.body.authCode;
+    const [username, token] = authCode.split(" ");
 
-    getSaves: async function (req, res) {
-        const authCode = req.body.authCode;
-        const [username, token] = authCode.split(' ');
+    await tryCatch(
+      async () => {
+        const decodedToken = decryptToken(token);
 
-        await tryCatch(
-            async () => {
-                const decodedToken = decryptToken(token);
+        if (decodedToken.username !== username) {
+          log(
+            "Username in the token does not match the provided username",
+            "error"
+          );
+          return [
+            400,
+            {
+              error:
+                "Username in the token does not match the provided username",
+            },
+          ];
+        }
 
-                if (decodedToken.username !== username) {
-                    log("Username in the token does not match the provided username", 'error');
-                    return [400, { error: "Username in the token does not match the provided username" }];
-                }
+        const uId = decodedToken.uid;
 
-                const uId = decodedToken.uid;
-
-                const query = `
+        const query = `
       SELECT saveId, lvl, time, money, cpuId, gpuId, ramId, stgId, lastBought
       FROM savedata
       WHERE userId = ?
       ORDER BY lastModified DESC
     `;
-                const savesResults = await executeQuery(query, uId, res, 'Player found!');
-
-                if (savesResults[1].data.length === 0) {
-                    log("User doesn't have saves!", 'info');
-                    return [404, { message: "User doesn't have saves!" }];
-                }
-
-                log("Saves retrieved successfully", 'success');
-                return [savesResults[0], { message: "Saves retrieved successfully", data: savesResults[1].data }];
-            },
-            "Error fetching saves",
-            res
+        const savesResults = await executeQuery(
+          query,
+          uId,
+          res,
+          "Player found!"
         );
-    },
 
-    setSavesOrUpdate: async function (req, res) {
-        const authCode = req.body.authCode;
-        const [username, token] = authCode.split(' ');
+        if (savesResults[1].data.length === 0) {
+          log("User doesn't have saves!", "info");
+          return [404, { message: "User doesn't have saves!" }];
+        }
 
-        await tryCatch(
-            async () => {
-                const decodedToken = decryptToken(token);
-                if (decodedToken.username !== username) {
-                    log("Username in the token does not match the provided username", 'error');
-                    return [400, { error: "Username in the token does not match the provided username" }];
-                }
-                
-                const uId = decodedToken.uid;
-                const savesData = req.body.data;
+        log("Saves retrieved successfully", "success");
+        return [
+          savesResults[0],
+          {
+            message: "Saves retrieved successfully",
+            data: savesResults[1].data,
+          },
+        ];
+      },
+      "Error fetching saves",
+      res
+    );
+  },
 
-                const appType = req.headers['x-save-type'];
-                switch (appType) {
-                    case null:
-                        for (const save of savesData) {
-                            const saveId = save.saveId;
-                            const existingSave = await checkExistingSave(uId, saveId, res);
-        
-                            if (existingSave) {
-                                // Save exists 
-                                log(`Save with ID ${saveId} already exists`, 'info');
-                                return [450, { message: `Would you like to override your  existing save with this name?`}]
-                                
-                            } else {
-                                // Create new save
-                                await createSave(uId, saveId, res);
-                                log(`Save with ID ${saveId} created successfully`, 'success');
-                            }
-                        }
-                        break;
-                    case "update":
-                        for (const save of savesData) {
-                            const saveId = save.saveId;
-                            const existingSave = await checkExistingSave(uId, saveId, res);
-        
-                            if (existingSave) {
-                                // Update existing save
-                                await updateSave(uId, save, res);
-                                log(`Save with ID ${saveId} updated successfully`, 'success');
-                            } else {
-                                // Create new save
-                                await createSaveIfNotExists(uId, save, res);
-                                log(`Save with ID ${saveId} created successfully`, 'success');
-                            }
-                        };
-                        break;
-                    case "override":
-                        const overrideQuery = `UPDATE savedata SET lvl=0,money=0,time=0,cpuId=0,gpuId=0,ramId=0,stgId=0,lastBought='{\"cpu\":0,\"gpu\":0,\"ram\":0,\"stg\":0}' WHERE userId = ? AND saveId = ?`;
-                        const overrideValues = [savesData.saveId, uId];
-                        await executeQuery(overrideQuery, overrideValues, 'Overriding existing savefile', res, 'Successful override');
-                        log(`Save with ID ${saveId} updated successfully`, 'success');
-                        break;
-                    default:
-                        break;
-                }
-                
+  setSavesOrUpdate: async function (req, res) {
+    const authCode = req.body.authCode;
+    const [username, token] = authCode.split(" ");
 
-                log("Saves updated or created successfully", 'success');
-                return [200, { message: "Saves updated or created successfully" }];
+    await tryCatch(
+      async () => {
+        const decodedToken = decryptToken(token);
+        if (decodedToken.username !== username) {
+          log(
+            "Username in the token does not match the provided username",
+            "error"
+          );
+          return [
+            400,
+            {
+              error:
+                "Username in the token does not match the provided username",
             },
-            "Error updating or creating saves",
-            res
+          ];
+        }
+
+        const uId = decodedToken.uid;
+        const savesData = req.body.data;
+
+        const appType = req.headers["x-save-type"];
+        switch (appType) {
+          case null:
+            for (const save of savesData) {
+              const saveId = save.saveId;
+              const existingSave = await checkExistingSave(uId, saveId, res);
+
+              if (existingSave) {
+                // Save exists
+                log(`Save with ID ${saveId} already exists`, "info");
+                return [
+                  450,
+                  {
+                    message: `Would you like to override your  existing save with this name?`,
+                  },
+                ];
+              } else {
+                // Create new save
+                await createSave(uId, saveId, res);
+                log(`Save with ID ${saveId} created successfully`, "success");
+              }
+            }
+            break;
+          case "update":
+            for (const save of savesData) {
+              const saveId = save.id;
+              const existingSave = await checkExistingSave(uId, saveId, res);
+
+              if (existingSave) {
+                // Update existing save
+                await updateSave(uId, save, res);
+                log(`Save with ID ${saveId} updated successfully`, "success");
+              } else {
+                // Create new save
+                await createSaveIfNotExists(uId, save, res);
+                log(`Save with ID ${saveId} created successfully`, "success");
+              }
+            }
+            break;
+          case "override":
+            const overrideQuery = `UPDATE savedata SET lvl=0,money=0,time=0,cpuId=0,gpuId=0,ramId=0,stgId=0,lastBought='{\"cpu\":0,\"gpu\":0,\"ram\":0,\"stg\":0}' WHERE userId = ? AND saveId = ?`;
+            const overrideValues = [savesData.saveId, uId];
+            await executeQuery(
+              overrideQuery,
+              overrideValues,
+              "Overriding existing savefile",
+              res,
+              "Successful override"
+            );
+            log(`Save with ID ${saveId} updated successfully`, "success");
+            break;
+          default:
+            break;
+        }
+
+        log("Saves updated or created successfully", "success");
+        return [200, { message: "Saves updated or created successfully" }];
+      },
+      "Error updating or creating saves",
+      res
+    );
+  },
+  deleteSave: async function (req, res) {
+    const { authCode, saveId } = req.body;
+    const [username, token] = authCode.split(" ");
+
+    await tryCatch(
+      async () => {
+        const decodedToken = decryptToken(token);
+
+        if (decodedToken.username !== username) {
+          log(
+            "Username in the token does not match the provided username",
+            "error"
+          );
+          return [
+            400,
+            {
+              error:
+                "Username in the token does not match the provided username",
+            },
+          ];
+        }
+
+        const uId = decryptToken(token).uid;
+        const deleteQuery = `DELETE FROM savedata WHERE userId = ? AND saveId = ?`;
+        const values = [uId, saveId];
+        const results = await executeQuery(
+          deleteQuery,
+          values,
+          "Player save successfully deleted",
+          res,
+          "Saves deleted successfully"
         );
-    },
-    deleteSave: async function (req, res) {
-        const {authCode, saveId} = req.body;
-        const [username, token] = authCode.split(' ');
-
-        await tryCatch(
-            async () => {
-                const decodedToken = decryptToken(token);
-
-                if (decodedToken.username !== username) {
-                    log("Username in the token does not match the provided username", 'error');
-                    return [400, { error: "Username in the token does not match the provided username" }];
-                }
-                
-                const uId = decryptToken(token).uid;
-                const deleteQuery = `DELETE FROM savedata WHERE userId = ? AND saveId = ?`;
-                const values = [uId, saveId];
-                const results = await executeQuery(deleteQuery, values, "Player save successfully deleted", res, 'Saves deleted successfully')
-                return results;
-            },
-            "Error during save delete",
-            res
-        )
-    }
+        return results;
+      },
+      "Error during save delete",
+      res
+    );
+  },
 };
 
 module.exports = { gameController };
