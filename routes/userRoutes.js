43726003ae83b19c70eb15485b2f08a2b3673c40
Balogh Levente <baloghm.levente@gmail.com>
2024-02-29 09:37:06 +0100

    refactor start with  playercontroller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { playerController } = require('../controllers/userController');
+const { registerPlayer } = require('../controllers/userController');
+const {registerPlayerSchema} = require('../sharedFunctions/validationSchemas')
 
 
-router.post('/register', playerController.registerPlayer);
-router.post('/login', playerController.loginUser);
-router.post('/forgotPassword', playerController.forgotPassword);
-router.post('/resetPassword', playerController.resetPassword);
+router.post('/register', registerPlayerSchema, (req, res) => registerPlayer(req, res));
+// router.post('/login', playerController.loginUser);
+// router.post('/forgotPassword', playerController.forgotPassword);
+// router.post('/resetPassword', playerController.resetPassword);
 
 
 module.exports = router;
--- sharedFunctions/functions.js
+++ sharedFunctions/functions.js
@@ -8,182 +8,189 @@ class ApiResponse {
   }
 }
 
-class ErrorHandler {
-  static handle(res, error, defaultMessage = "An error occurred") {
-    const errorMessage = (error instanceof Error) ? error.message : defaultMessage;
-    log(errorMessage, 'error');
-    return ApiResponse.send(res, 500, { error: errorMessage });
-  }
-}
+// function log(message, type) {
+//   const colors = {
+//     info: "\x1b[34m", // Blue
+//     success: "\x1b[32m", // Green
+//     error: "\x1b[31m", // Red
+//   };
 
-function log(message, type) {
-  const colors = {
-    info: "\x1b[34m", // Blue
-    success: "\x1b[32m", // Green
-    error: "\x1b[31m", // Red
-  };
+//   const resetColor = "\x1b[0m";
+//   const color = colors[type] || resetColor; // Default to white
 
-  const resetColor = "\x1b[0m";
-  const color = colors[type] || resetColor; // Default to white
+//   const formattedTimestamp = `[${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}] - `;
+//   const coloredMessage = `${color}${formattedTimestamp}${formatMessage(message)}${resetColor}`;
 
-  const formattedTimestamp = `[${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}] - `;
-  const coloredMessage = `${color}${formattedTimestamp}${formatMessage(message)}${resetColor}`;
+//   console.log(coloredMessage);
+// }
 
-  console.log(coloredMessage);
-}
+// function formatMessage(message) {
+//   if (Array.isArray(message)) {
+//     return formatArray(message);
+//   } else if (typeof message === 'object' && message !== null) {
+//     return formatObject(message);
+//   } else {
+//     return message;
+//   }
+// }
 
-function formatMessage(message) {
-  if (Array.isArray(message)) {
-    return formatArray(message);
-  } else if (typeof message === 'object' && message !== null) {
-    return formatObject(message);
-  } else {
-    return message;
-  }
-}
+// function formatArray(arr) {
+//   return `[${arr.map(formatMessage).join(', ')}]`;
+// }
 
-function formatArray(arr) {
-  return `[${arr.map(formatMessage).join(', ')}]`;
-}
-
-function formatObject(obj) {
-  const entries = Object.entries(obj).map(([key, value]) => `${key}: ${formatMessage(value)}`);
-  return `{ ${entries.join(', ')} }`;
-}
+// function formatObject(obj) {
+//   const entries = Object.entries(obj).map(([key, value]) => `${key}: ${formatMessage(value)}`);
+//   return `{ ${entries.join(', ')} }`;
+// }
 
 
-async function tryCatch(errorMessage, res, successMessage = "Operation successful", callback) {
-  try {
-    const result = await callback();
-    log(successMessage, "success");
-    ApiResponse.send(res, result[0], result[1]);
-  } catch (error) {
-    const logMessage = errorMessage || "An error occurred";
-    log(logMessage, 'error');
-    console.error(error);
-    ErrorHandler.handle(res, error, logMessage);
-  }
-}
-
-async function executeQuery(query, values, logMessage, res, successMessage) {
+async function executeQuery(query, values) {
   const connection = await db.pool.getConnection();
   try {
     const [results] = await connection.query(query, values);
-    log(logMessage + "\n" + `Database query successful: ${query}`, "success");
-    return [200, { message: successMessage, data: results }];
+    return  results;
   } finally {
     connection.release();
   }
 }
 
-const checkUserExists = async (field, value, errorMessage, res) => {
-  const result = await executeQuery(`SELECT * FROM userTbl WHERE ${field} = ?`, [value], `Query to check ${field}`, res, "");
-  log(`Query results for ${field} check:\n${JSON.stringify(result[1].data)}`, "info");
-  if (result[1].data.length === 0) {
-    return [404, { error: errorMessage }];
-  }
-
-  return result[1].data[0];
-};
-
-const handleApplicationLogin = async (username, password, res) => {
-  const user = await checkUserExists("username", username, "User not found", res);
-  if (user[0] == 404) {
-    return user;
-  }
-
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    return [401, { error: "Invalid password" }];
-  }
-
-  return [
-    200,
-    {
-      message: "Login successful",
-      data: [user.username, createToken(user)]
-    }
-  ];
-};
-
-const handleWebsiteLogin = async (username, password, res) => {
-  const user = await checkUserExists("username", username, "User not found", res);
-  if (user[0] == 404) {
-    return user;
-  }
-
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    return [401, { error: "Invalid password" }];
-  }
-
-  return [
-    200,
-    {
-      message: "Login successful",
-      data: [user.username, createToken(user), user.password,]
-    }
-  ];
-};
-
-const updateUserField = async (field, newValue, conditionField, conditionValue, successMessage, res) => {
-  await executeQuery(`UPDATE userTbl SET ${field} = ? WHERE ${conditionField} = ?`, [newValue, conditionValue], `Query to update ${field}`, res, successMessage);
-};
-
-const checkExistingSave = async (uId, saveId, res) => {
-  const result = await executeQuery(`SELECT * FROM savedata WHERE userId = ? AND saveId = ?`, [uId, saveId], `Query to check existing save`, res, "");
-  return result[1].data.length > 0;
-};
-
-const updateSave = async (uId, save, res) => {
-  const updateQuery = `
-      UPDATE savedata
-      SET lvl = ?, time = ?, money = ?, cpuId = ?, gpuId = ?, ramId = ?, stgId = ?, lastBought = ?
-      WHERE userId = ? AND saveId = ?
-  `;
-
-  const updateValues = [
-    save.lvl, save.time, save.money, save.cpu, save.gpu, save.ram, save.stg, save.lb,
-    uId, save.id
-  ];
-
-  await executeQuery(updateQuery, updateValues, `Query to update save`, res, "Save updated successfully");
+async function generateHash(password){
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword
 }
 
-const createSave = async (uId, saveId, res) => {
-  const insertQuery = `CALL createFirstSaveFile(?,?)`;
-
-  const insertValues = [
-    saveId, uId
-  ];
-
-  await executeQuery(insertQuery, insertValues, `Query to create save`, res, "Save created successfully");
-};
-
-const createSaveIfNotExists = async (uId, save, res) => {
-  const insertQuery = `
-      INSERT INTO savedata (userId, saveId, lvl, time, money, cpuId, gpuId, ramId, stgId, lastBought)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-
-  const insertValues = [
-    uId, save.saveId, save.lvl, save.time, save.money, save.cpuId, save.gpuId, save.ramId, save.stgId, save.lastBought
-  ];
-
-  await executeQuery(insertQuery, insertValues, `Query to create save`, res, "Save created successfully");
+async function compareHash(password, hashedPassword){
+  const passwordMatch = await bcrypt.compare(password, hashedPassword);
+  return passwordMatch
 }
 
-module.exports = {
-  ErrorHandler,
-  tryCatch,
-  executeQuery,
-  log,
-  checkUserExists,
-  handleApplicationLogin,
-  handleWebsiteLogin,
-  updateUserField,
-  checkExistingSave,
-  updateSave,
-  createSave,
-  createSaveIfNotExists
-};
+
+module.exports = {executeQuery, generateHash, compareHash}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const checkUserExists = async (field, value, errorMessage, res) => {
+//   const result = await executeQuery(`SELECT * FROM userTbl WHERE ${field} = ?`, [value], `Query to check ${field}`, res, "");
+//   log(`Query results for ${field} check:\n${JSON.stringify(result[1].data)}`, "info");
+//   if (result[1].data.length === 0) {
+//     return [404, { error: errorMessage }];
+//   }
+
+//   return result[1].data[0];
+// };
+
+// const handleApplicationLogin = async (username, password, res) => {
+//   const user = await checkUserExists("username", username, "User not found", res);
+//   if (user[0] == 404) {
+//     return user;
+//   }
+
+//   const passwordMatch = await bcrypt.compare(password, user.password);
+//   if (!passwordMatch) {
+//     return [401, { error: "Invalid password" }];
+//   }
+
+//   return [
+//     200,
+//     {
+//       message: "Login successful",
+//       data: [user.username, createToken(user)]
+//     }
+//   ];
+// };
+
+// const handleWebsiteLogin = async (username, password, res) => {
+//   const user = await checkUserExists("username", username, "User not found", res);
+//   if (user[0] == 404) {
+//     return user;
+//   }
+
+//   const passwordMatch = await bcrypt.compare(password, user.password);
+//   if (!passwordMatch) {
+//     return [401, { error: "Invalid password" }];
+//   }
+
+//   return [
+//     200,
+//     {
+//       message: "Login successful",
+//       data: [user.username, createToken(user), user.password,]
+//     }
+//   ];
+// };
+
+// const updateUserField = async (field, newValue, conditionField, conditionValue, successMessage, res) => {
+//   await executeQuery(`UPDATE userTbl SET ${field} = ? WHERE ${conditionField} = ?`, [newValue, conditionValue], `Query to update ${field}`, res, successMessage);
+// };
+
+// const checkExistingSave = async (uId, saveId, res) => {
+//   const result = await executeQuery(`SELECT * FROM savedata WHERE userId = ? AND saveId = ?`, [uId, saveId], `Query to check existing save`, res, "");
+//   return result[1].data.length > 0;
+// };
+
+// const updateSave = async (uId, save, res) => {
+//   const updateQuery = `
+//       UPDATE savedata
+//       SET lvl = ?, time = ?, money = ?, cpuId = ?, gpuId = ?, ramId = ?, stgId = ?, lastBought = ?
+//       WHERE userId = ? AND saveId = ?
+//   `;
+
+//   const updateValues = [
+//     save.lvl, save.time, save.money, save.cpu, save.gpu, save.ram, save.stg, save.lb,
+//     uId, save.id
+//   ];
+
+//   await executeQuery(updateQuery, updateValues, `Query to update save`, res, "Save updated successfully");
+// }
+
+// const createSave = async (uId, saveId, res) => {
+//   const insertQuery = `CALL createFirstSaveFile(?,?)`;
+
+//   const insertValues = [
+//     saveId, uId
+//   ];
+
+//   await executeQuery(insertQuery, insertValues, `Query to create save`, res, "Save created successfully");
+// };
+
+// const createSaveIfNotExists = async (uId, save, res) => {
+//   const insertQuery = `
+//       INSERT INTO savedata (userId, saveId, lvl, time, money, cpuId, gpuId, ramId, stgId, lastBought)
+//       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+//   `;
+
+//   const insertValues = [
+//     uId, save.saveId, save.lvl, save.time, save.money, save.cpuId, save.gpuId, save.ramId, save.stgId, save.lastBought
+//   ];
+
+//   await executeQuery(insertQuery, insertValues, `Query to create save`, res, "Save created successfully");
+// }
+
+// module.exports = {
+//   executeQuery,
+
+//   checkUserExists,
+//   handleApplicationLogin,
+//   handleWebsiteLogin,
+//   updateUserField,
+//   checkExistingSave,
+//   updateSave,
+//   createSave,
+//   createSaveIfNotExists
+// };
+
+module.exports = ApiResponse
